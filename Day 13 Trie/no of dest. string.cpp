ef0e36c6fec7576203d1bd7137928bbf49ee6133
raaz-harsh <xrn004@gmail.com>
2023-07-11 05:35:37 +0530

    Day 13 done

---
+++ Day 13 Trie/no of dest. string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+int distinctSubstring(string &word) {
+    //  Write your code here.
+     unordered_map<string,int> mp;
+
+    for(int i = 0; i < word.length(); i++){
+        for(int j = i; j < word.length(); j++){
+            mp[word.substr(i,j-i+1)]++;
+        }
+    }
+
+    return mp.size();
+}
